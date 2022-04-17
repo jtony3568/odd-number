ecbcbfac9649fa81bc6d3a694821e81e738c93f4
jtony3568 <jtony3568@gmail.com>
2022-04-17 18:10:01 +0530

    new path

---
+++ src/org/test/Oddnumber.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class Oddnumber {
+  public static void main(String[] args) {
+    for (int i = 1; i <= 10; i++) {
+      if (i%1 == 5) {
+      }
+      System.out.println(i);
+
+    }
+
+  }
+
+}
